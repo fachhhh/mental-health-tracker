3d74b08c72774d0d0c8c641e6779d3b6c67f9eb0
fachhhh <fachhhhs.01@gmail.com>
2024-08-28 22:24:21 +0700

    modified on allowed host by added URL deployment PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h%c9xw&sa^prg#m-f0+0me-w-d#6s=#s4%2$h1s13_26q46yko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "hadyan-fachri-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
